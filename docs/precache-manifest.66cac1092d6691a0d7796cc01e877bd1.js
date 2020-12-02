c78419ee201da0d0df7766a950145473aa593be5
Angel Rodriguez Aguirre <rodriguezaguirreangelt@gmail.com>
2020-12-01 23:13:42 -0600

    Docs agregado

---
+++ docs/precache-manifest.66cac1092d6691a0d7796cc01e877bd1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "409ac4c43cb9a7e1d9979a6de4cecedc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6ded81a0258c285e29d8",
+    "url": "/static/css/main.cbe2d6d9.chunk.css"
+  },
+  {
+    "revision": "17511b471bfdde6e14c9",
+    "url": "/static/js/2.ee48284c.chunk.js"
+  },
+  {
+    "revision": "f5fc2afae58c4ffc30c6148864765d4b",
+    "url": "/static/js/2.ee48284c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ded81a0258c285e29d8",
+    "url": "/static/js/main.aec48648.chunk.js"
+  },
+  {
+    "revision": "0a62db85d9147c438712",
+    "url": "/static/js/runtime-main.2bd8def6.js"
+  }
+]);
